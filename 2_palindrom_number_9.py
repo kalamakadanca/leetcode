619906b443714bd9a8339e668c9a1d66d5f3cc04
kalamakadanca <kalamakadanca@seznam.cz>
2024-05-12 01:34:20 +0200

    f

--- 2_palindrom_number_9.py
+++ 2_palindrom_number_9.py
@@ -1,2 +1,27 @@
+#Given an integer x, return true if x is a palindrome, and false otherwise.
+
+
 def isPalindrome(x):
-    f
+    #x = len(x) / 2
+    for i in range(x//2):
+        x = str(x)
+        print(x[i])
+        print(i)
+        print(x[-i])
+        print(-i)
+        if x[i] != x[-1]:
+            return False
+    return True
+
+
+
+
+
+
+
+print(isPalindrome(121))
+#true
+print(isPalindrome(-121))
+#false
+print(isPalindrome(10))
+#false
